cf0450ade484908cc64b029c0bae344b53e3cbbe
shimetsu3 <mateusrmartins01@gmail.com>
2025-08-13 00:05:07 -0300

    continuação calendário

--- src/pages/calendar/index.js
+++ src/pages/calendar/index.js
@@ -1,14 +1,26 @@
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, Image, TouchableOpacity, StyleSheet } from 'react-native';
 
 export function Calendar() {
     return (
-        <View style={{flex: 1, backgroundColor: 'white'}}>
+        <View style={{flex: 1, backgroundColor: 'red'}}>
             
-        </View>
+        </View>,
+        <text> aaaa</text>
     );
 }
 
 const styles= StyleSheet.create({
+    header:{
+        height: "10%",
+        flexDirection: "row",
+        alignItems: "center",
+        justifyContent: "space-between",
+        margin: 10,
 
+    },
+
+    cabecalho:{
+        marginleft: 10
+    }
 
 });
