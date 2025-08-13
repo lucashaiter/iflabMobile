5f442b4727e84908e397b690a10a20c8cb72aeb2
shimetsu3 <mateusrmartins01@gmail.com>
2025-08-13 00:09:04 -0300

    continuação calendário

--- src/pages/calendar/index.js
+++ src/pages/calendar/index.js
@@ -3,13 +3,20 @@ import { View, Text, Image, TouchableOpacity, StyleSheet } from 'react-native';
 export function Calendar() {
     return (
         <View style={{flex: 1, backgroundColor: 'red'}}>
-            
+            <View styles={styles.header}>
+                <Image
+                    style={styles.cabecalho}
+                    source={require('../../assets/icons/UI/left.png')}
+                />
+                <text>aaaa</text>
+            </View>
+            <text>aaa</text>
         </View>,
         <text> aaaa</text>
     );
 }
 
-const styles= StyleSheet.create({
+const styles = StyleSheet.create({
     header:{
         height: "10%",
         flexDirection: "row",
