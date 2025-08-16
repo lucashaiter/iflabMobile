43824a9055a6effa4ac37338c74aa6e30c676135
BiancaLochetti <biancalochetti@gmail.com>
2025-08-16 16:04:08 -0300

    .

--- App.js
+++ App.js
@@ -2,14 +2,14 @@ import { NavigationContainer } from '@react-navigation/native'
 import { Routes } from './src/routes'
 import LoginScreen from './src/pages/login'; 
 
-export default function App() {
+/*export default function App() {
   return <LoginScreen />;
-}
+}*/
 
-/*export default function App(){
+export default function App(){
   return(
     <NavigationContainer>
       <Routes/>
     </NavigationContainer>
   )
-}*/
+}
