c1a8964e7ff30bdccdf37e7e45ba939433b220d5
BiancaLochetti <biancalochetti@gmail.com>
2025-08-16 16:02:34 -0300

    login updates

--- App.js
+++ App.js
@@ -1,10 +1,15 @@
 import { NavigationContainer } from '@react-navigation/native'
 import { Routes } from './src/routes'
+import LoginScreen from './src/pages/login'; 
 
-export default function App(){
+export default function App() {
+  return <LoginScreen />;
+}
+
+/*export default function App(){
   return(
     <NavigationContainer>
       <Routes/>
     </NavigationContainer>
   )
-}
+}*/
